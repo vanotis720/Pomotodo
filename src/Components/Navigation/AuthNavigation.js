edfd86dced464cff0ecabd097cf3e76cea615c1d
vanotis720 <vanotis720@gmail.com>
2023-03-28 18:43:07 +0200

    started screen ui and finished

--- src/Components/Navigation/AuthNavigation.js
+++ src/Components/Navigation/AuthNavigation.js
@@ -5,12 +5,21 @@ import Tasks from '../Screens/Tasks';
 import Timer from '../Screens/Timer';
 import { LoginView } from '../Screens/Login';
 import { RegisterView } from '../Screens/Register';
+import { StartedView } from '../Screens/Started';
 
 const Stack = createNativeStackNavigator();
 
 export default function AuthNavigation() {
     return (
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="Started">
+            <Stack.Screen
+                name="Started"
+                component={StartedView}
+                options={{
+                    headerShown: false,
+                    title: 'Pomotodo - authentification'
+                }}
+            />
             <Stack.Screen
                 name="Login"
                 component={LoginView}
